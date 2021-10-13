9df68b0dd86be7e801705cfea7b9bea1d0555054
Peter Hutterer <peter.hutterer@who-t.net>
2021-10-13 14:45:23 +1000

    Make media-session its own standalone package
    This removes the rest of pipewire from the tree, but keeps the git repo.
    The package name is now just 'media-session' but the binary remains
    pipewire-media-session for backwards compatibility.
    
    No functional changes besides the removal but a few renames for
    variables from PIPEWIRE_FOO to MEDIA_SESSION_FOO (and its lowercase
    version).
    
    In docs/ folder, the media-session.dox moved to be the new mainpage
    index.dox and all other dox files are removed.
    
    pw-uninstalled.sh becomes media-session-uninstalled.
    
    Finally, the hierarchy was flattened, so
    - src/media-session/* goes into src/
    - src/daemon/systemd goes into systemd/
    - src/media-session/media-session.d goes into media-session.d

---
+++ media-session-uninstalled.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+while getopts ":b:v:" opt; do
+	case ${opt} in
+		b)
+			BUILDDIR=${OPTARG}
+			;;
+		v)
+			VERSION=${OPTARG}
+			echo "Version: ${VERSION}"
+			;;
+		\?)
+			echo "Invalid option: -${OPTARG}"
+			exit 1
+			;;
+		:)
+			echo "Option -${OPTARG} requires an argument"
+			exit 1
+			;;
+	esac
+done
+
+if [ -z "${BUILDDIR}" ]; then
+	BUILDDIR=${SCRIPT_DIR}/builddir
+	echo "Using default build directory: ${BUILDDIR}"
+fi
+
+if [ ! -d "${BUILDDIR}" ]; then
+	echo "Invalid build directory: ${BUILDDIR}"
+	exit 1
+fi
+
+# the config file read by the daemon
+export MEDIA_SESSION_CONFIG_DIR="${BUILDDIR}/media-session.d"
+export PATH="${BUILDDIR}/src:${PATH}"
+
+export PW_UNINSTALLED=1
+export PKG_CONFIG_PATH="${BUILDDIR}/meson-uninstalled/:${PKG_CONFIG_PATH}"
+
+# FIXME: find a nice, shell-neutral way to specify a prompt
+${SHELL}
--- src/modules/module-protocol-pulse/message-handler.h
+++
@@ -1,8 +0,0 @@
-#ifndef PULSE_SERVER_MESSAGE_HANDLER_H
-#define PULSE_SERVER_MESSAGE_HANDLER_H
-
-struct pw_manager_object;
-
-void register_object_message_handlers(struct pw_manager_object *o);
-
-#endif /* PULSE_SERVER_MESSAGE_HANDLER_H */
