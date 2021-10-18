1ca8988a63693a457b2a585628a52d34b623dc75
Peter Hutterer <peter.hutterer@who-t.net>
2021-10-18 11:29:35 +1000

    media-session: use the SESSION_PREFIX for any fallback config dir
    Regression introduced in 3560f3ba2d41a8596c40ee3894fe2484039aac47
    
    MEDIA_SESSION_CONFIG_PATH expects the actual directory while
    PIPEWIRE_CONFIG_PATH and the built-in fallback both expect the files to
    reside within the SESSION_PREFIX subdirectory.
    
    Fixes #1725
    
    (cherry picked from commit dfb63d55dddabb6cc211880fca3974d9c2c297ba)

--- src/media-session.c
+++ src/media-session.c
@@ -2368,9 +2368,10 @@ static int collect_modules(struct impl *impl, const char *str)
 	int count = 0;
 
 	dir = getenv("MEDIA_SESSION_CONFIG_DIR");
-	if (dir == NULL && (dir = getenv("PIPEWIRE_CONFIG_DIR")) == NULL) {
-		dir = MEDIA_SESSION_CONFDATADIR;
+	if (dir == NULL) {
 		prefix = SESSION_PREFIX;
+		if ((dir = getenv("PIPEWIRE_CONFIG_DIR")) == NULL)
+			dir = MEDIA_SESSION_CONFDATADIR;
 	}
 	if (dir == NULL)
 		return -ENOENT;
